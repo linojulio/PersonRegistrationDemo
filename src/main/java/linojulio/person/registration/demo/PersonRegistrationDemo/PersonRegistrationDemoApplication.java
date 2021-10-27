a77908225cd8870fa95b14ec958f0bbe7904dae1
Julio Lino <afonsojuliounifesp@gmail.com>
2021-10-27 00:43:13 -0300

    project kickoff

---
+++ src/main/java/linojulio/person/registration/demo/PersonRegistrationDemo/PersonRegistrationDemoApplication.java
@@ -0,0 +1,13 @@
+package linojulio.person.registration.demo.PersonRegistrationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegistrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegistrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/linojulio/person/registration/demo/PersonRegistrationDemo/PersonRegistrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package linojulio.person.registration.demo.PersonRegistrationDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRegistrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
