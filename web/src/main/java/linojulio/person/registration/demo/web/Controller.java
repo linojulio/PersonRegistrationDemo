cd9cb7e89ba47c291347e04ef90932b328d01723
Julio Lino <afonsojuliounifesp@gmail.com>
2021-10-27 19:56:52 -0300

    dependency config ok

--- web/src/main/java/linojulio/person/registration/demo/web/Controller.java
+++ web/src/main/java/linojulio/person/registration/demo/web/Controller.java
@@ -1,9 +1,6 @@
 package linojulio.person.registration.demo.web;
 
-import linojulio.person.registration.demo.usecase.UsecaseClass;
-
 public class Controller {
     public void TestController() {
-        UsecaseClass
     }
 }
